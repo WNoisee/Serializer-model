f496293f8e50cd202c97a99119107a12acce4179
WNoisee <europe8paris@gmail.com>
2023-08-02 09:58:14 +0700

    serializer

--- tutorial/serializers.py
+++ tutorial/serializers.py
@@ -11,5 +11,17 @@ class SnippetSerializer(serializers.Serializer):
     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
 
-
-        
+    def create(self, validated_data):
+        return Snippet.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance, given the validated data.
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.code = validated_data.get('code', instance.code)
+        instance.linenos = validated_data.get('linenos', instance.linenos)
+        instance.language = validated_data.get('language', instance.language)
+        instance.style = validated_data.get('style', instance.style)
+        instance.save()
+        return instance
