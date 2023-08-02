3c8d1d30597f7eeab2744a67f415a0631d721721
WNoisee <europe8paris@gmail.com>
2023-08-02 14:42:59 +0700

    ok

--- tutorial/serializers.py
+++ tutorial/serializers.py
@@ -2,7 +2,6 @@
 from rest_framework import serializers
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
-
 class SnippetSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(required=False, allow_blank=True, max_length=100)
